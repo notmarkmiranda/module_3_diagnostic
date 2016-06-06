cea9e9211a44e0748e7cb63fa5d0fabc11ddb2bd
Mark Miranda <notmarkmiranda@gmail.com>
2016-06-06 10:25:28 -0600

    working on current path with correct params

--- spec/features/user_searches_zip_code_spec.rb
+++ spec/features/user_searches_zip_code_spec.rb
@@ -9,8 +9,11 @@ RSpec.feature "zip code search", type: :feature do
     # And I fill in the search form with 80203
     fill_in "q", with: "80203"
     # And I click "Locate"
-    click_button "Locate"
+    click_on "Locate"
     # Then I should be on page "/search?zip=80203"
+    uri = URI.parse(current_url)
+    "#{uri.path}?#{uri.query}".should == people_path(:search => 'name')
+
     expect(current_path).to eq "/search?zip=80203"
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     expect(Station.count).to eq 10
